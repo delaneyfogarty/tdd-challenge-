c67e107b0610ecbfadc6b768abe542c0f0bdd65d
Delaney Fogarty <delaney.fk@gmail.com>
2022-03-10 12:49:45 -0800

    passing text for function multiplyBySeven

--- test/functions.test.js
+++ test/functions.test.js
@@ -31,24 +31,35 @@ test('this test should add exclamation points to the strings', (expect) => {
 
     const actual = addExclamationPoints('bunny rabbit' + '!!!');
 
-    expect.equal(actual, expected, 'this return will add three exclamation points to bunny rabbits');
+    expect.equal(actual, expected);
 
     const expected2 = 'boi george!!!!';
     const actual2 = addExclamationPoints('boi george' + '!!!!');
 
-    expect.equal(actual2, expected2, 'this will return four exclamation points to boi george');
+    expect.equal(actual2, expected2);
 
     const expected3 = 'princey boi!!!!!';
     const actual3 = addExclamationPoints('princey boi' + '!!!!!');
 
-    expect.equal(actual3, expected3, 'this will return five exclamation points after princey boi');
+    expect.equal(actual3, expected3, 'this will return exclamation points after the string');
 
 });
 
 test('this test should multiply a given number by 7', (expect) => {
-    const expected = 
+    const expected = 28;
 
-})
+    const actual = multiplyBySeven(4);
+
+    expect.equal(actual, expected);
+
+    const expected2 = 49;
+    const actual2 = multiplyBySeven(7);
+    expect.equal(actual2, expected2);
+
+    const expected3 = 21;
+    const actual3 = multiplyBySeven(3);
+    expect.equal(actual3, expected3, 'this will return the product of a chosen number and 7');
+});
 
 
 
